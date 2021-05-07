97802e84e38d8255d45bbb419a91fa97114822ae
ismaelpacheco13 <ismaelpacheco13001@gmail.com>
2021-05-07 11:25:37 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Inicio from "./components/Inicio";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">
+            Inicio
+          </Link>
+          <Link to="/login" className="btn btn-dark">
+            Login
+          </Link>
+          <Link to="/dashboard" className="btn btn-dark">
+            Dashboard
+          </Link>
+        </div>
+        <hr />
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Inicio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
